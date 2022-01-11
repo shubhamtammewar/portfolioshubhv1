31f99f87d855930a4dd9114aee5c496c658a25e4
shubhamtammewar <91666990+shubhamtammewar@users.noreply.github.com>
2022-01-11 19:09:54 +0530

    Update SideIcons.jsx

--- src/Components/SideIcons/SideIcons.jsx
+++ src/Components/SideIcons/SideIcons.jsx
@@ -57,7 +57,7 @@ const SideIcons = () => {
 							</svg>
 						</a>
 					</li>
-					<li>
+					{/* <li>
 						<a
 							href='/'
 							aria-label='Instagram'
@@ -87,8 +87,8 @@ const SideIcons = () => {
 								<line x1='17.5' y1='6.5' x2='17.51' y2='6.5' />
 							</svg>
 						</a>
-					</li>
-					<li>
+					</li> */}
+					{/* <li>
 						<a
 							href='/'
 							aria-label='Twitter'
@@ -109,7 +109,7 @@ const SideIcons = () => {
 								<path d='M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z' />
 							</svg>
 						</a>
-					</li>
+					</li> */}
 				</div>
 			</div>
 		</div>
